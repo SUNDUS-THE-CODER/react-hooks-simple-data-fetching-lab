febf65ab3c5ad6e2cbdbeae36f06b534d85bf4c9
SUNDUS-THE-CODER <sundusahmed648@gmail.com>
2023-03-06 13:54:18 +0000

    display loading text and fetched image

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,36 @@
-// create your App component here
+import React, { useEffect, useState } from 'react'
+
+const App = () => {
+    const [loading, setLoading] = useState(false);
+    const [image, setImage] = useState(null);
+    const fetchData = async () => {
+        try {
+            setLoading(true);
+            let data = await fetch('https://dog.ceo/api/breeds/image/random');
+            data = await data.json();
+            setImage(data.message);
+            setLoading(false);
+        } catch (error) {
+            setLoading(false);
+            console.log(error.message);
+        }
+    }
+    useEffect(() => {
+      fetchData();
+      return () => null;
+    }, []);
+  return (
+    <div style={{ 
+        minHeight: '100vh', 
+        display: 'flex', 
+        alignItems: 'center', 
+        justifyContent: 'center'
+    }}>
+        {loading ? <p>Loading...</p> : (
+            <img src={image} alt="A Random Dog" style={{ width: '400px', height: '400px' }} />
+        )}
+    </div>
+  )
+}
+
+export default App
